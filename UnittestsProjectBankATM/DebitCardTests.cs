e2d2dfdb9ae8e1fa3ae256c363a3f63ba6e90a94
Robinsilverio <robinsilverio17@gmail.com>
2022-01-01 05:59:05 +0100

    Unittests for class DebitCard written and the tests went successful

---
+++ UnittestsProjectBankATM/DebitCardTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankATMApp;
+
+namespace UnittestsProjectBankATM
+{
+    [TestClass]
+    public class DebitCardTests
+    {
+        [TestMethod]
+        public void Should_ReturnTrue_WhenUserIsAuthorizedUsingPIN()
+        {
+            DebitCard testSubjectDebitCard = new DebitCard(0001, "Robin Medeiros Silvério", 1234);
+            
+            Assert.IsTrue(testSubjectDebitCard.VerifyPIN(1234));
+        }
+
+        [TestMethod]
+        public void Should_ReturnFalse_WhenUserIsUnauthorizedUsingWrongPIN()
+        {
+            DebitCard testSubjectDebitCard = new DebitCard(0001, "Robin Medeiros Silvério", 0123);
+            
+            Assert.IsFalse(testSubjectDebitCard.VerifyPIN(1234));
+        }
+    }
+}
